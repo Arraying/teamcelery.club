bf0c7ea149a424b76998b9600e59bbb7609e8ba7
Arraying <paul.huebner@googlemail.com>
2021-05-21 17:17:52 +0200

    Add JavaScript to make navbar responsive.

---
+++ assets/navbar.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    const burgers = document.querySelectorAll('.navbar-burger');
+    burgers.forEach(burger => {
+        burger.addEventListener('click', () => {
+            const targetId = burger.dataset.target;
+            const target = document.getElementById(targetId);
+            burger.classList.toggle('is-active');
+            target.classList.toggle('is-active');
+        });
+    });
+};
